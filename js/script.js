f3839ed595defb6482095679bc51a2fa99228f65
Adrian <adisw123@gmail.com>
2018-06-05 16:17:13 +0200

    Summary
    Solo website

--- js/script.js
+++ js/script.js
@@ -37,3 +37,43 @@ $(document).ready(function () {
         animation: 'slide'
     });
 });
+/*======== Portfolio =========*/
+$(window).on('load', function () {
+
+    // Initialize Isotope
+    $("#isotope-container").isotope({
+
+    });
+
+    // filter items on button click
+    $("#isotope-filters").on('click', 'button', function () {
+
+        // get filter value
+        var filterValue = $(this).attr('data-filter');
+
+        // filter portfolio
+        $("#isotope-container").isotope({
+            filter: filterValue
+        });
+
+        //active button
+        $('#isotope-filters').find('.active').removeClass('active');
+        $(this).addClass('active')
+    });
+});
+/* =========================================
+               Magnifier
+============================================ */
+
+$(function(){
+
+    $("#portfolio-wrapper").magnificPopup({
+        delegate:'a',
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+
+    })
+
+});
